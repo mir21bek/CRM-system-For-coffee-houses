dc8075dd9d89cff37c4ebf31ab073bc289610d96
mir21bek <mirbek.2121@gmail.com>
2024-02-20 16:05:08 +0600

    rewrited config for celery/ rewwrited tasks/updated admin.py

--- apps/user_app/tasks.py
+++ apps/user_app/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from .models import CustomUser
 from utils.services.send_email import generate_code_for_send_mail
+from config.settings import email
 
 
 @shared_task
@@ -11,5 +12,7 @@ def user_created(user_id):
     massage = "Dear {}\n\nyou have successfully registered, please confirm your e-mail. Enter the 4-digit code below {}".format(
         user.full_name, generate_code_for_send_mail()
     )
-    mail_send = send_mail(subject, massage, "vanilla_sky@gmail.com", [user.email])
-    return mail_send
+    to_email = user.email
+    from_email = email.EMAIL_HOST_USER
+    send_mail(subject, massage, [from_email], [to_email], fail_silently=False)
+    return user
