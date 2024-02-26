7a9b6d079c758d577aad59926ecff77f88422b91
mir21bek <mirbek.2121@gmail.com>
2024-02-26 10:03:54 +0600

    added login serializer for users

--- apps/user_app/serializers.py
+++ apps/user_app/serializers.py
@@ -23,7 +23,34 @@ class CheckOtpSerializer(serializers.ModelSerializer):
         fields = ("otp",)
 
 
-class LoginSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ("email",)
+class BaseLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, help_text="Введите адрес эл. почты")
+
+    def validate_role(self, value):
+        if value not in CustomUser.ROLE:
+            raise serializers.ValidationError("Недопустимая роль")
+        return value
+
+
+class ClientLoginSerializer(BaseLoginSerializer):
+    def create(self, validated_data):
+        validated_data["role"] = "client"
+        return CustomUser.objects.create(**validated_data)
+
+
+class AdminLoginSerializer(BaseLoginSerializer):
+    def create(self, validated_data):
+        validated_data["role"] = "admin"
+        return CustomUser.objects.create(**validated_data)
+
+
+class BaristaLoginSerializer(BaseLoginSerializer):
+    def create(self, validated_data):
+        validated_data["role"] = "barista"
+        return CustomUser.objects.create(**validated_data)
+
+
+class WaiterLoginSerializer(BaseLoginSerializer):
+    def create(self, validated_data):
+        validated_data["role"] = "barista"
+        return CustomUser.objects.create(**validated_data)
